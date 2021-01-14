efeaecca8bc3a6412fc8b3477d32cb91b54e3d8e
Nikhil Math <erulemath@gmail.com>
2021-01-13 18:32:24 -0700

    delteed gemlock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::API
 	include DeviseTokenAuth::Concerns::SetUserByToken
 	# before_action :authenticate_user!
-	before_action :configure_permitted_parameters, if: :devise_controller?
 
-	protected 
-		# params.require(:sign_up).permit(:email, :password)
-		# params.require(:sign_up).permit(:email, :password..., :address, :phone)
-		def configure_permitted_parameters 
-			devise_parameter_sanitizer.permit(:sign_up, keys: [:address, :phone])
-		end
 end
