a5f323e5f450a390fd721e7c64c38f4ff820f5e5
Sydne Hanks <sydnehanks@gmail.com>
2021-01-14 21:25:27 -0700

    updated update

--- app/controllers/api/pets_controller.rb
+++ app/controllers/api/pets_controller.rb
@@ -48,11 +48,41 @@ class Api::PetsController < ApplicationController
   end
 
   def update
-    if @pet.update(pet_params)
-      render json: @pet
+    @pet = current_user.pets.new()
+    @pet.nombre = params[:nombre]
+    @pet.age = params [:age]
+    @pet.animal = params [:animal]
+    @pet.color = params [:color]
+    @pet.weight = params [:weight]
+    @pet.sex = params [:sex]
+    @pet.service = params [:service]
+    file = params [:file]
+    if file && file != ""
+      begin
+        ext = File.extname(file.tempfile)
+        cloud_image = Cloudinary::Uploader.upload(
+          file, 
+          public_id: file.original_filename, 
+          secure: true
+        )
+        @pet.pic = cloud_image['secure_url']
+        if @pet.save
+          render json: @pet
+        else
+          render json: { errors: @pet.errors }, status: :unprocessable_entity
+        end
+      rescue => e
+        render json: { errors: e }, status: 422
+      end
     else
-      render json: { errors: @pet.errors }, status: :unprocessable_entity
+      @pet.avatar = 'https://images.unsplash.com/photo-1493916665398-143bdeabe500?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8YW5pbWFsc3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
+      if @pet.save
+        render json: @pet
+      else
+        render json: { errors: @pet.errors }, status: :unprocessable_entity
+      end
     end
+
   end
 
 
