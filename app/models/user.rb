65c448b062a02297834f4c69fb741dbf814b542a
Parker Jones <parker.harold.jones@gmail.com>
2021-01-27 20:21:57 -0700

    Added dependent destroy code

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
-  has_many :pets
+  has_many :pets, dependent: :destroy
 end
