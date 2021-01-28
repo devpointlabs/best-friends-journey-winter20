c47dcd371cf67cc77eeee3d0cb5866f1dd6b6158
Mel Fabella <mf820am@gmail.com>
2021-01-27 19:53:19 -0800

    added petlist

---
+++ client/src/components/pet/PetList.js
@@ -0,0 +1,19 @@
+import { List } from 'semantic-ui-react';	
+import { Link } from 'react-router-dom';	
+import Pet from './Pet';	
+const PetList = ({ pets, deletePet }) => (	
+  <List divided relaxed>	
+    <List.Item>	
+      <List.Content>	
+        { pets.map( p => 	
+          <Pet	
+            petId={p.id}	
+            {...p} 	
+            deletePet={deletePet}	
+          />	
+        )}	
+      </List.Content>	
+    </List.Item> 	
+  </List>	
+)	
+export default PetList; 
