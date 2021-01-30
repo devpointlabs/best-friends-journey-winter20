0be1bc7df1f5eef20ccb380696b07b631bc579f8
Nikhil Math <erulemath@gmail.com>
2021-01-29 20:07:49 -0700

    updated navbar and made about page about developers

---
+++ client/src/components/shared/Developers.js
@@ -0,0 +1,46 @@
+const Developers = () => (
+  <>
+    <h1>The Development Team</h1>
+    <h2>Mel Fabella</h2>
+    {/* <img src="#" alt="Picture of Mel Fabella"> */}
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    <p>email</p>
+    <div>
+      <a href="https://www.linkedin.com/in/m-fabella/">LinkedIn</a>
+      <br></br>
+      <a href="https://github.com/MelFabella">GitHub</a>
+    </div>
+
+    <h2>Sydne Hanks</h2>
+    {/* <img src="#" alt="Picture of Sydne Hanks"> */}
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    <p>email</p>
+    <div>
+      <a href="https://www.linkedin.com/in/sydnehanks/">LinkedIn</a>
+      <br></br>
+      <a href="https://github.com/sydnehanks">GitHub</a>
+    </div>
+
+    <h2>Parker Jones</h2>
+    {/* <img src="#" alt="Picture of Parker Jones"> */}
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    <p>email</p>
+    <div>
+      <a href="https://www.linkedin.com/in/parkerhjones/">LinkedIn</a>
+      <br></br>
+      <a href="https://github.com/ParkerHJones">GitHub</a>
+    </div>
+
+    <h2>Nikhil Math</h2>
+    {/* <img src="#" alt="Picture of Nikhil Math"> */}
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+    <p>email</p>
+    <div>
+    <a href="https://www.linkedin.com/in/nikhil-math/">LinkedIn</a>
+    <br></br>
+    <a href="https://github.com/NikhilMath">GitHub</a>
+    </div>
+  </>
+)
+
+export default Developers;
