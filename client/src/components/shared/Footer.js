fde5ba6ec9c8d569f7dde470d451daf7701f4439
Sydne Hanks <sydnehanks@gmail.com>
2021-02-03 19:46:14 -0700

    Navbar edited

---
+++ client/src/components/shared/Footer.js
@@ -0,0 +1,8 @@
+import { FooterContainer } from '../stylecomponents/NavStyles';
+
+const Footer = () => (
+  <FooterContainer>
+    <p> &#169; 2021 DevPoint Studio LLC </p>
+  </FooterContainer>
+)
+export default Footer;
